939fa4555e511d3d65d401fad1bde3f69cf8f676
Ques-Mendes <quesmendes@gmail.com>
2022-05-16 20:21:19 -0300

    Added validation for login

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
+const crypto = require('crypto');
+
+const middlewares = require('./middlewares');
+
 const route = express.Router();
 
-route.post('/login', (req, res) => {
+// https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js
+function generateToken() {
+  return crypto.randomBytes(8).toString('hex');
+}
 
+route.post('/', middlewares.loginValidation, (_req, res) => {
+  const token = generateToken();
+  res.status(200).json({ token });
 });
 
 module.exports = route;
